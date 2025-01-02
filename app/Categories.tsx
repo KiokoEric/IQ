7032b3788cfbecde810be976621e93ddc5d8fe08
KiokoEric <kiokoerick040@gmail.com>
2025-01-02 17:29:39 +0300

    Deletion of the Errors and setErrors useState in the Categories section of the IQ mobile application.

--- app/Categories.tsx
+++ app/Categories.tsx
@@ -1,15 +1,11 @@
 import { Link } from 'expo-router';
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import { SelectList } from 'react-native-dropdown-select-list';
 import { useAppContext } from '@/components/AppContext/AppContext';
 const Categories = () => {
 
-    // USESTATE
-
-    const [Errors, setErrors] = useState<string>("")
-
-    // USEAPP CONTEXT
+    // USE APP CONTEXT
 
     const { setCategory, setDifficulty, Type, setType  } = useAppContext()
 
@@ -46,51 +42,52 @@ const Categories = () => {
     ]
 
 return (
-<View style={{ backgroundColor: 'white', display: 'flex', flexDirection: 'column', rowGap: 20, alignItems: 'center', justifyContent: 'center', height: 800  }} >
-    <Text style={{ color: 'black', fontFamily: 'InstrumentSerif', fontSize: 25, marginTop: 20, textTransform:'capitalize', textDecorationLine: 'underline'  }} >Select your quiz parameters</Text>
-    <View style={{ display: 'flex', flexDirection: 'column', rowGap: 2, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ color: 'black', fontFamily: 'InstrumentSerif', fontSize: 20,  textDecorationLine: 'underline' }} >Category</Text>
-        <SelectList 
-            setSelected={(value: any) => { setCategory(value) }} 
-            data={Categorydata} 
-            save="key"
-            boxStyles= {{ borderBottomColor: 'black', width: 300 }}
-            inputStyles={{ fontFamily: 'InstrumentSerif', fontSize: 18 }}
-            dropdownTextStyles={{ fontFamily: 'InstrumentSerif', fontSize: 18, outline: 'none' }}
-        />
-    </View>
-    <View style={{ display: 'flex', flexDirection: 'column', rowGap: 2, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ color: 'black', fontFamily: 'InstrumentSerif', fontSize: 20,  textDecorationLine: 'underline' }}>Difficulty</Text>
-        <SelectList 
-            setSelected={(value: any) => { setDifficulty(value) }} 
-            data={Difficultydata} 
-            save="key"
-            boxStyles= {{ borderBottomColor: 'black', width: 300 }}
-            inputStyles={{ fontFamily: 'InstrumentSerif', fontSize: 18, outline: 'none' }}
-            dropdownTextStyles={{ fontFamily: 'InstrumentSerif', fontSize: 18 }}
-        />
-    </View>
-    <View style={{ display: 'flex', flexDirection: 'column', rowGap: 2, alignItems: 'center', justifyContent: 'center', marginBottom: 25 }}>
-        <Text style={{ color: 'black', fontFamily: 'InstrumentSerif', fontSize: 20,  textDecorationLine: 'underline' }}>Type</Text>
-        <SelectList 
-            setSelected={(value: any) => { setType(value) }}
-            data={Typedata}
-            save="key"
-            boxStyles= {{ borderBottomColor: 'black', width: 300 }}
-            inputStyles={{ fontFamily: 'InstrumentSerif', fontSize: 18, outline: 'none' }}
-            dropdownTextStyles={{ fontFamily: 'InstrumentSerif', fontSize: 18 }}
-        />
-    </View>
-    <Text style={{ color: 'black', fontFamily: 'InstrumentSerif', fontSize: 18 }}>{Errors}</Text>
-    <Link href={'/Questions'} style={{ backgroundColor: 'black', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', paddingVertical: 10, paddingHorizontal: 10, borderRadius: 5, minWidth: 180 }} >
-        <Text style={{ color: 'white', fontFamily: 'InstrumentSerif', fontSize: 18, textAlign: 'center'}}>Submit Parameters</Text>
-    </Link>
-    <View style={{ display: 'flex', flexDirection: 'row', gap: 10, marginTop: 60 }}>
-        <Link href={'/'} style={{ backgroundColor: 'black', display: 'flex', flexDirection: 'column', alignItems: 'center', paddingVertical: 10, paddingHorizontal: 10, borderRadius: 5, minWidth: 180 }}>
-            <Text style={{ color: 'white', fontFamily: 'InstrumentSerif', fontSize: 18, textAlign: 'center' }}>Return to Home</Text>
+<ScrollView>
+    <View style={{ backgroundColor: 'white', display: 'flex', flexDirection: 'column', rowGap: 20, alignItems: 'center', justifyContent: 'center', height: 800  }} >
+        <Text style={{ color: 'black', fontFamily: 'InstrumentSerif', fontSize: 25, marginTop: 20, textTransform:'capitalize', textDecorationLine: 'underline'  }} >Select your quiz parameters</Text>
+        <View style={{ display: 'flex', flexDirection: 'column', rowGap: 2, alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{ color: 'black', fontFamily: 'InstrumentSerif', fontSize: 20,  textDecorationLine: 'underline' }} >Category</Text>
+            <SelectList 
+                setSelected={(value: any) => { setCategory(value) }} 
+                data={Categorydata} 
+                save="key"
+                boxStyles= {{ borderBottomColor: 'black', width: 300 }}
+                inputStyles={{ fontFamily: 'InstrumentSerif', fontSize: 18 }}
+                dropdownTextStyles={{ fontFamily: 'InstrumentSerif', fontSize: 18, outline: 'none' }}
+            />
+        </View>
+        <View style={{ display: 'flex', flexDirection: 'column', rowGap: 2, alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{ color: 'black', fontFamily: 'InstrumentSerif', fontSize: 20,  textDecorationLine: 'underline' }}>Difficulty</Text>
+            <SelectList 
+                setSelected={(value: any) => { setDifficulty(value) }} 
+                data={Difficultydata} 
+                save="key"
+                boxStyles= {{ borderBottomColor: 'black', width: 300 }}
+                inputStyles={{ fontFamily: 'InstrumentSerif', fontSize: 18, outline: 'none' }}
+                dropdownTextStyles={{ fontFamily: 'InstrumentSerif', fontSize: 18 }}
+            />
+        </View>
+        <View style={{ display: 'flex', flexDirection: 'column', rowGap: 2, alignItems: 'center', justifyContent: 'center', marginBottom: 25 }}>
+            <Text style={{ color: 'black', fontFamily: 'InstrumentSerif', fontSize: 20,  textDecorationLine: 'underline' }}>Type</Text>
+            <SelectList 
+                setSelected={(value: any) => { setType(value) }}
+                data={Typedata}
+                save="key"
+                boxStyles= {{ borderBottomColor: 'black', width: 300 }}
+                inputStyles={{ fontFamily: 'InstrumentSerif', fontSize: 18, outline: 'none' }}
+                dropdownTextStyles={{ fontFamily: 'InstrumentSerif', fontSize: 18 }}
+            />
+        </View>
+        <Link href={'/Questions'} style={{ backgroundColor: 'black', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', paddingVertical: 10, paddingHorizontal: 10, borderRadius: 5, minWidth: 180 }} >
+            <Text style={{ color: 'white', fontFamily: 'InstrumentSerif', fontSize: 18, textAlign: 'center'}}>Submit Parameters</Text>
         </Link>
+        <View style={{ display: 'flex', flexDirection: 'row', gap: 10, marginTop: 60 }}>
+            <Link href={'/'} style={{ backgroundColor: 'black', display: 'flex', flexDirection: 'column', alignItems: 'center', paddingVertical: 10, paddingHorizontal: 10, borderRadius: 5, minWidth: 180 }}>
+                <Text style={{ color: 'white', fontFamily: 'InstrumentSerif', fontSize: 18, textAlign: 'center' }}>Return to Home</Text>
+            </Link>
+        </View>
     </View>
-</View>
+</ScrollView>
 )
 }
 
--- app/Questions.tsx
+++ app/Questions.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 import Button from '@/components/Common/Button/Button';
 import Answer from '@/components/Common/Answer/Answer';
 const Loading = require('../assets/images/Loading.jpg');
-import { StyleSheet, View, Text, Image } from 'react-native';
+import { StyleSheet, View, Text, Image, ScrollView } from 'react-native';
 import { useAppContext } from '@/components/AppContext/AppContext';
 
 const Questions = () => {
@@ -94,49 +94,51 @@ const Questions = () => {
     const Options = Questions[currentQuestion]
 
 return (
-<View>
-    {   
-    isLoading ? (
-        <View style={{ backgroundColor: 'white', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', margin: 'auto', height: 800 }} >
-            <Image source={Loading} style={styles.Loading}  />
-        </View>
-    ) :
-    (
-    <View style= {{ backgroundColor: 'white',display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', rowGap: 20, minHeight: 800, paddingHorizontal: 10, paddingVertical: 10 }} >
-        <Text style={{ fontFamily: 'InstrumentSerif', fontSize: 25, textAlign: 'center' }}>Category: {Questions[currentQuestion].category}</Text>
-        <Text style={{ fontFamily: 'InstrumentSerif', fontSize: 25, textAlign: 'center' }}>Question Number: {currentQuestion + 1} / {Questions.length}</Text>
-        <View>
-            <Text style={{ fontFamily: 'InstrumentSerif', fontSize: 25, textAlign: 'center' }}>{Questions[currentQuestion].question}</Text>
-        </View>
-        <View style={{ display: 'flex', flexDirection: 'column', rowGap: 20 }}>
-        { 
-            [...Questions[currentQuestion].incorrect_answers, Questions[currentQuestion].correct_answer].sort().map((answer:any, index: number) => (
-                <Answer 
-                    key={index}
-                    onPress={() => handleAnswer(answer)}
-                    AnswerText={answer}
-                    disabled={selectedAnswer !== ''}
+<ScrollView>
+    <View>
+        {   
+        isLoading ? (
+            <View style={{ backgroundColor: 'white', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', margin: 'auto', height: 800 }} >
+                <Image source={Loading} style={styles.Loading}  />
+            </View>
+        ) :
+        (
+        <View style= {{ backgroundColor: 'white',display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', rowGap: 20, minHeight: 800, paddingHorizontal: 10, paddingVertical: 10 }} >
+            <Text style={{ fontFamily: 'InstrumentSerif', fontSize: 25, textAlign: 'center' }}>Category: {Questions[currentQuestion].category}</Text>
+            <Text style={{ fontFamily: 'InstrumentSerif', fontSize: 25, textAlign: 'center' }}>Question Number: {currentQuestion + 1} / {Questions.length}</Text>
+            <View>
+                <Text style={{ fontFamily: 'InstrumentSerif', fontSize: 25, textAlign: 'center' }}>{Questions[currentQuestion].question}</Text>
+            </View>
+            <View style={{ display: 'flex', flexDirection: 'column', rowGap: 20 }}>
+            { 
+                [...Questions[currentQuestion].incorrect_answers, Questions[currentQuestion].correct_answer].sort().map((answer:any, index: number) => (
+                    <Answer 
+                        key={index}
+                        onPress={() => handleAnswer(answer)}
+                        AnswerText={answer}
+                        disabled={selectedAnswer !== ''}
+                    />
+                )) 
+            }
+            </View>
+            <View style={{ marginTop: 20 }} >
+                <Button 
+                    onPress={handleNextQuestion}
+                    ButtonText='Next Question'
                 />
-            )) 
-        }
+            </View>
+            <View style={{ display: 'flex', flexDirection: 'row', gap: 10, marginTop: 20 }}>
+                <Link href={'/'} style={{ backgroundColor: 'black', display: 'flex', flexDirection: 'column', alignItems: 'center', paddingVertical: 10, paddingHorizontal: 10, borderRadius: 5, minWidth: 180 }}>
+                    <Text style={{ color: 'white', fontFamily: 'InstrumentSerif', fontSize: 20, textAlign: 'center' }}>Return to Home</Text>
+                </Link>
+                <Link href={'/Categories'} style={{ backgroundColor: 'black', display: 'flex', flexDirection: 'column', alignItems: 'center', paddingVertical: 10, paddingHorizontal: 10, borderRadius: 5, minWidth: 180 }}>
+                    <Text style={{ color: 'white', fontFamily: 'InstrumentSerif', fontSize: 20, textAlign: 'center' }} >Quiz Settings</Text>
+                </Link>
+            </View>
         </View>
-        <View style={{ marginTop: 20 }} >
-            <Button 
-                onPress={handleNextQuestion}
-                ButtonText='Next Question'
-            />
-        </View>
-        <View style={{ display: 'flex', flexDirection: 'row', gap: 10, marginTop: 20 }}>
-            <Link href={'/'} style={{ backgroundColor: 'black', display: 'flex', flexDirection: 'column', alignItems: 'center', paddingVertical: 10, paddingHorizontal: 10, borderRadius: 5, minWidth: 180 }}>
-                <Text style={{ color: 'white', fontFamily: 'InstrumentSerif', fontSize: 20, textAlign: 'center' }}>Return to Home</Text>
-            </Link>
-            <Link href={'/Categories'} style={{ backgroundColor: 'black', display: 'flex', flexDirection: 'column', alignItems: 'center', paddingVertical: 10, paddingHorizontal: 10, borderRadius: 5, minWidth: 180 }}>
-                <Text style={{ color: 'white', fontFamily: 'InstrumentSerif', fontSize: 20, textAlign: 'center' }} >Quiz Settings</Text>
-            </Link>
-        </View>
+        )}
     </View>
-    )}
-</View>
+</ScrollView>
 )
 }
 
